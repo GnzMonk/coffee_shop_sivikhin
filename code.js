111d4a3d9f5bc72424e5a1357f9c72804a81219f
Vladislav Sivikhin <sivlad004@yandex.ru>
2025-12-06 13:42:16 +0300

    add order page

--- code.js
+++ code.js
@@ -1,33 +1,224 @@
-coffee_list = [
+const coffee_list = [
+    // Cappuccino (6 объектов)
     {
-    "name": "Latte",
-    "type": "latte",
-    "short": 99,
-    "tall": 139,
-    "grande": 159
+        "name": "Classic Cappuccino",
+        "type": "cappuccino",
+        "short": 89,
+        "tall": 119,
+        "grande": 139
     },
-        {
-    "name": "Latte",
-    "type": "latte",
-    "short": 99,
-    "tall": 139,
-    "grande": 159
+    {
+        "name": "Vanilla Cappuccino",
+        "type": "cappuccino",
+        "short": 99,
+        "tall": 129,
+        "grande": 149
     },
-        {
-    "name": "Latte",
-    "type": "latte",
-    "short": 99,
-    "tall": 139,
-    "grande": 159
+    {
+        "name": "Caramel Cappuccino",
+        "type": "cappuccino",
+        "short": 99,
+        "tall": 129,
+        "grande": 149
     },
-        {
-    "name": "Latte",
-    "type": "latte",
-    "short": 99,
-    "tall": 139,
-    "grande": 159
+    {
+        "name": "Hazelnut Cappuccino",
+        "type": "cappuccino",
+        "short": 99,
+        "tall": 129,
+        "grande": 149
+    },
+    {
+        "name": "Cinnamon Cappuccino",
+        "type": "cappuccino",
+        "short": 94,
+        "tall": 124,
+        "grande": 144
+    },
+    {
+        "name": "Double Shot Cappuccino",
+        "type": "cappuccino",
+        "short": 109,
+        "tall": 139,
+        "grande": 159
+    },
+
+    // Latte (6 объектов)
+    {
+        "name": "Classic Latte",
+        "type": "latte",
+        "short": 99,
+        "tall": 139,
+        "grande": 159
+    },
+    {
+        "name": "Vanilla Latte",
+        "type": "latte",
+        "short": 109,
+        "tall": 149,
+        "grande": 169
+    },
+    {
+        "name": "Caramel Latte",
+        "type": "latte",
+        "short": 109,
+        "tall": 149,
+        "grande": 169
+    },
+    {
+        "name": "Matcha Latte",
+        "type": "latte",
+        "short": 119,
+        "tall": 159,
+        "grande": 179
+    },
+    {
+        "name": "Hazelnut Latte",
+        "type": "latte",
+        "short": 109,
+        "tall": 149,
+        "grande": 169
+    },
+    {
+        "name": "Coconut Latte",
+        "type": "latte",
+        "short": 114,
+        "tall": 154,
+        "grande": 174
+    },
+
+    // Americano (6 объектов)
+    {
+        "name": "Classic Americano",
+        "type": "americano",
+        "short": 79,
+        "tall": 99,
+        "grande": 119
+    },
+    {
+        "name": "Iced Americano",
+        "type": "americano",
+        "short": 79,
+        "tall": 99,
+        "grande": 119
+    },
+    {
+        "name": "Long Black Americano",
+        "type": "americano",
+        "short": 84,
+        "tall": 104,
+        "grande": 124
+    },
+    {
+        "name": "Americano Misto",
+        "type": "americano",
+        "short": 89,
+        "tall": 109,
+        "grande": 129
+    },
+    {
+        "name": "Double Americano",
+        "type": "americano",
+        "short": 94,
+        "tall": 114,
+        "grande": 134
+    },
+    {
+        "name": "Americano with Cream",
+        "type": "americano",
+        "short": 89,
+        "tall": 109,
+        "grande": 129
+    },
+
+    // Espresso (исправлен typo "expresso" на "espresso" согласно CoffeeType)
+    {
+        "name": "Single Espresso",
+        "type": "expresso",
+        "short": 69,
+        "tall": 89,
+        "grande": 109
+    },
+    {
+        "name": "Double Espresso",
+        "type": "expresso",
+        "short": 89,
+        "tall": 109,
+        "grande": 129
+    },
+    {
+        "name": "Ristretto",
+        "type": "expresso",
+        "short": 74,
+        "tall": 94,
+        "grande": 114
+    },
+    {
+        "name": "Lungo",
+        "type": "expresso",
+        "short": 74,
+        "tall": 94,
+        "grande": 114
+    },
+    {
+        "name": "Espresso Macchiato",
+        "type": "expresso",
+        "short": 79,
+        "tall": 99,
+        "grande": 119
+    },
+    {
+        "name": "Espresso Con Panna",
+        "type": "expresso",
+        "short": 84,
+        "tall": 104,
+        "grande": 124
+    },
+
+    // Flat White (6 объектов)
+    {
+        "name": "Classic Flat White",
+        "type": "flatWhite",
+        "short": 94,
+        "tall": 124,
+        "grande": 144
+    },
+    {
+        "name": "Double Ristretto Flat White",
+        "type": "flatWhite",
+        "short": 104,
+        "tall": 134,
+        "grande": 154
+    },
+    {
+        "name": "Vanilla Flat White",
+        "type": "flatWhite",
+        "short": 104,
+        "tall": 134,
+        "grande": 154
+    },
+    {
+        "name": "Caramel Flat White",
+        "type": "flatWhite",
+        "short": 104,
+        "tall": 134,
+        "grande": 154
+    },
+    {
+        "name": "Coconut Flat White",
+        "type": "flatWhite",
+        "short": 109,
+        "tall": 139,
+        "grande": 159
+    },
+    {
+        "name": "Honey Flat White",
+        "type": "flatWhite",
+        "short": 99,
+        "tall": 129,
+        "grande": 149
     }
-]
+];
 
 const CoffeeType = {
     cappuccino: "cappuccino",
